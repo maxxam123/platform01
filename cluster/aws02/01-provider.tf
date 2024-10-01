3472ac64bb814cf9edd50e7ba7049cce0ad768a9
maxxam123 <hennighausenmax@gmail.com>
2024-10-01 09:14:13 +0000

    Update from https://github.com/maxxam123/platform02/commit/d75e8424345a6fc2fbcd3e8c7c7fa198a35839d9

--- cluster/aws02/01-provider.tf
+++ cluster/aws02/01-provider.tf
@@ -1,11 +1,11 @@
-provider "ami-01e444924a2233b07" {
+provider "aws" {
   region = "eu-central-1"
 }
 
 terraform {
   backend "s3" {
     bucket         = "dunhuang-gansu-terraform-state"
-    key            = "dunhuang/terraform/s3/ami-01e444924a2233b0702/terraform.tfstate"
+    key            = "dunhuang/terraform/s3/aws02/terraform.tfstate"
     region         = "eu-central-1"
     dynamodb_table = "terraform_state"
   }
