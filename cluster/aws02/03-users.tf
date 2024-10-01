678bf6e2ad80365e12350ba83f13de8e28c28c50
maxxam123 <hennighausenmax@gmail.com>
2024-10-01 14:15:19 +0000

    Update from https://github.com/maxxam123/platform02/commit/cc465989074994e73466f5d6618737e1ced02be1

---
+++ cluster/aws02/03-users.tf
@@ -0,0 +1,15 @@
+resource "aws_iam_user" "nick" {
+  name = "nick"
+}
+resource "aws_iam_user" "lukas" {
+  name = "lukas"
+}
+resource "aws_iam_user" "michael" {
+  name = "michael"
+}
+resource "aws_iam_user" "luisa" {
+  name = "luisa"
+}
+resource "aws_iam_user" "marry" {
+  name = "marry"
+}
