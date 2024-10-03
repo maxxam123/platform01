0815eced5635382a7730d301731697782810dd07
max <81824845+maxxam123@users.noreply.github.com>
2024-10-03 08:13:29 -0700

    Update main.tf

--- modules/aws/main.tf
+++ modules/aws/main.tf
@@ -1,8 +1,12 @@
-resource "aws_instance" "vm01" {
-  ami = var.ami
-  instance_type = var.instance_type
-  subnet_id = var.subnet_id
-  tags = {
-    Name = var.tag
-  }
+#resource "aws_instance" "vm01" {
+#  ami = var.ami
+#  instance_type = var.instance_type
+#  subnet_id = var.subnet_id
+#  tags = {
+#    Name = var.tag
+#  }
+#}
+
+resource "aws_iam_user" "demouser" {
+      name = var.tag
 }
