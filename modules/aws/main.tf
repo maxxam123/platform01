80164af2744cd6bb969c6bf79609c9ed096a7d6c
max <81824845+maxxam123@users.noreply.github.com>
2024-10-03 23:46:29 -0700

    Update main.tf

--- modules/aws/main.tf
+++ modules/aws/main.tf
@@ -1,7 +1,7 @@
 module "iam_iam-user" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-user"
   version = "5.45.0"
-  name = NAME
+  name = var.name
 }
 
 #resource "aws_instance" "vm01" {
