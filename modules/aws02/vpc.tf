e1b8c4a3590c2a9f311924c9cf84fdafea985da7
max <81824845+maxxam123@users.noreply.github.com>
2024-10-05 05:31:20 -0700

    Update vpc.tf

--- modules/aws02/vpc.tf
+++ modules/aws02/vpc.tf
@@ -8,7 +8,7 @@ module "vpc" {
   # name = var.name
   # cidr = var.cidr
 
-  azs = [eu-central-1a, eu-central-1b]
+  azs = ["eu-central-1a", "eu-central-1b"]
   private_subnets = ["10.0.0.0/19", "10.0.32.0/19"]
   # private_subnets = [var.cidr01, var.cidr02]
   public_subnets  = ["10.0.64.0/19", "10.0.96.0/19"]
