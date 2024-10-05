390514f213e9a889fac55365db382cdbb98c85d2
max <81824845+maxxam123@users.noreply.github.com>
2024-10-05 03:57:51 -0700

    Update eks_post.tf

--- modules/aws02/eks_post.tf
+++ modules/aws02/eks_post.tf
@@ -134,3 +134,22 @@ module "eks" {
     Environment = "staging"
   }
 }
+
+data "aws_eks_cluster" "default" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "default" {
+  name = module.eks.cluster_id
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.default.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.default.id]
+    command     = "aws"
+  }
+}
