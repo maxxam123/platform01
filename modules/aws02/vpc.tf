b38625a01cbaf3342f692506817975984add3d51
max <81824845+maxxam123@users.noreply.github.com>
2024-10-05 03:36:19 -0700

    Create vpc.tf

---
+++ modules/aws02/vpc.tf
@@ -0,0 +1,27 @@
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.0.0"
+
+  name = "main"
+  cidr = "10.0.0.0/16"
+  # name = var.name
+  # cidr = var.cidr
+
+  azs = [var.azs01, var.azs02]
+  private_subnets = ["10.0.0.0/19", "10.0.32.0/19"]
+  # private_subnets = [var.cidr01, var.cidr02]
+  public_subnets  = ["10.0.64.0/19", "10.0.96.0/19"]
+  # public_subnets = [var.cidr03, var.cidr04]
+
+  enable_nat_gateway     = true
+  single_nat_gateway     = true
+  one_nat_gateway_per_az = false
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Environment = "staging"
+  }
+}
