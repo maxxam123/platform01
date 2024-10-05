a12f42c6e9458248be069cc2950089fdbe2122e9
max <81824845+maxxam123@users.noreply.github.com>
2024-10-05 03:57:21 -0700

    Update eks_post.tf

--- modules/aws02/eks_post.tf
+++ modules/aws02/eks_post.tf
@@ -76,3 +76,61 @@ module "eks_admins_iam_group" {
   group_users                       = [module.user1_iam_user.iam_user_name]
   custom_group_policy_arns          = [module.allow_assume_eks_admins_iam_policy.arn]
 }
+
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "18.29.0"
+
+  cluster_name    = "NAME"
+  cluster_version = "1.23"
+
+  cluster_endpoint_private_access = true
+  cluster_endpoint_public_access  = true
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  enable_irsa = true
+
+  eks_managed_node_group_defaults = {
+    disk_size = 50
+  }
+
+  eks_managed_node_groups = {
+    general = {
+      desired_size = 1
+      min_size     = 1
+      max_size     = 3
+
+      labels = {
+        role = "general"
+      }
+      
+       instance_types = ["t3.small"]
+      capacity_type  = "ON_DEMAND"
+    }
+    
+    spot = {
+      desired_size = 1
+      min_size     = 1
+      max_size     = 3
+
+      labels = {
+        role = "spot"
+      }
+
+      taints = [{
+        key    = "market"
+        value  = "spot"
+        effect = "NO_SCHEDULE"
+      }]
+
+       instance_types = ["t3.micro"]
+      capacity_type  = "ON_DEMAND"
+    }
+  }  
+  tags = {
+    Environment = "staging"
+  }
+}
